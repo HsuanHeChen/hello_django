635bf6803fd151855da9cc5d2b91d40a71b6be60
HsuanHeChen <shane.chen@itrue.com.tw>
2017-11-14 18:23:52 +0800

    feat(Core): add App core, add model review to myapp.

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import TimeStampedModel
 
 # Create your models here.
+
+class Review(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    photo = models.URLField(blank=True)
+    location = models.CharField(blank=True, max_length=100)
+
+    def __str__(self):
+        return self.title
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +74,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "hello_django",
+        'USER': "root",
+        'PASSWORD': "1234",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
