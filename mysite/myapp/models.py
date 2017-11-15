0fab14e709d5a7514f1b2a4b97ba90a79cdc8e82
HsuanHeChen <shane.chen@itrue.com.tw>
2017-11-15 18:06:13 +0800

    feat(Product): add model Product to my app

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,13 +1,34 @@
 from django.db import models
+from django.utils import timezone
 from core.models import TimeStampedModel
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    use_for_related_fields = True
+
+    def published(self, **kwargs):
+        return self.filter(published_at__lte=timezone.now(), **kwargs)
+
+
+class Product(TimeStampedModel):
+    name = models.CharField(max_length=200)
+    sku =  models.CharField(blank=True, max_length=100)
+    photo = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.name
+
 
 class Review(TimeStampedModel):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     photo = models.URLField(blank=True)
     location = models.CharField(blank=True, max_length=100)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    objects = PublishedManager()
 
     def __str__(self):
         return self.title
