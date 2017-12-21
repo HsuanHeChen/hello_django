c9d4ec3eba69b4c8ad28fb886580b4b95f86391b
HsuanHeChen <shane.chen@itrue.com.tw>
2017-12-21 12:44:13 +0800

    TDD(ch17): About staging/production functional-test and I ignore it happily too.

--- mysite/functional_tests/test_my_lists.py
+++ mysite/functional_tests/test_my_lists.py
@@ -1,6 +1,5 @@
-from django.conf import settings
-from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
-from django.contrib.sessions.backends.db import SessionStore
+import time
+from django.contrib.auth import get_user_model
 from .base import FunctionalTest
 
 User = get_user_model()
@@ -9,19 +8,20 @@ User = get_user_model()
 class MyListsTest(FunctionalTest):
 
     def create_pre_authenticated_session(self):
-        user = User.objects.create_user(
+        User.objects.create_user(
             username='aaa',
             password='1234qwer',
             email='aaa@example.com'
         )
-        session = SessionStore()
-        session[SESSION_KEY] = user.pk
-        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-        session.save()
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_name('username').send_keys("aaa")
+        self.browser.find_element_by_name('password').send_keys("1234qwer")
+        self.browser.find_element_by_xpath('//input[@type="submit"]').click()
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.alert-success').text, 'Login successfully.'))
 
-        self.browser.get(self.server_url + "/404_no_such_url/")
-        self.browser.add_cookie(dict(
-            name=settings.SESSION_COOKIE_NAME,
-            value=session.session_key,
-            path='/'
-        ))
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        self.create_pre_authenticated_session()
+        self.browser.get(self.server_url)
+        self.wait_for(lambda: self.assertIn('aaa', self.browser.find_element_by_id('header-nav').text))
+        time.sleep(1)
